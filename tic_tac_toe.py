0840e57d6db4e4f288d6360fc2b288dcfd738fba
AutoPR <autopr@irgolic.com>
2023-04-09 14:42:20 +0000

    Implement display_board function

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,15 @@
 def display_board(board):
+def display_board(board):
+    for row in board:
+        print("|", end="")
+        for cell in row:
+            print(cell or " ", end="|")
+        print("\n" + "-" * 5)
+
+
+    board = [["" for _ in range(3)] for _ in range(3)]
+    display_board(board)
+
     pass
 
 def check_winner(board):
