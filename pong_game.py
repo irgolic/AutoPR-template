b971a41c072993f04ce12a8d04cd896b4db45afc
AutoPR <autopr@irgolic.com>
2023-04-12 13:50:08 +0000

    Implement the basic game loop in `pong_game.py`

--- pong_game.py
+++ pong_game.py
@@ -31,6 +31,27 @@ while running:
                 running = False
     
     # Update game logic
+    # Paddle movement
+    keys = pygame.key.get_pressed()
+    if keys[K_w]:
+        LEFT_PADDLE.move_ip(0, -5)
+    if keys[K_s]:
+        LEFT_PADDLE.move_ip(0, 5)
+    if keys[K_UP]:
+        RIGHT_PADDLE.move_ip(0, -5)
+    if keys[K_DOWN]:
+        RIGHT_PADDLE.move_ip(0, 5)
+
+    # Collision detection
+    LEFT_PADDLE.clamp_ip(screen.get_rect())
+    RIGHT_PADDLE.clamp_ip(screen.get_rect())
+
+    # Ball movement
+    BALL.move_ip(2, 2)
+    if BALL.colliderect(LEFT_PADDLE) or BALL.colliderect(RIGHT_PADDLE):
+        BALL.x *= -1
+    if BALL.y <= 0 or BALL.y >= SCREEN_HEIGHT - 15:
+        BALL.y *= -1
 
     # Redraw game objects
     screen.fill(0)
