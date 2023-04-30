4ce133fb7e44f975361f16d2a7660cc7edb8938b
Rafael Irgolic <hello@irgolic.com>
2023-04-30 20:38:26 +0200

    dice_rolling_bot: Add message_content intent

--- dice_rolling_bot/dice_rolling_bot.py
+++ dice_rolling_bot/dice_rolling_bot.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
